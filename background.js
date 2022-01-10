8ef7199129d0eee8cb718b6276e46cbf41f70739
yeonlisa <tkzw4869@gmail.com>
2022-01-10 20:18:48 +0900

    Background

--- background.js
+++ background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 
 document.body.appendChild(bgImage);
