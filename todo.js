33a8b14f38160f5e8706ccb4a2da23d44867e631
yeonlisa <tkzw4869@gmail.com>
2022-01-11 18:07:37 +0900

    Adding ToDos

--- todo.js
+++ todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
