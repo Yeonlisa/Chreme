86d084d25431b0b406a6f932ae7f714e46c3d398
yeonlisa <tkzw4869@gmail.com>
2022-01-11 18:24:58 +0900

    Saving To Dos

--- todo.js
+++ todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(e) {
     const li = e.target.parentElement;
     li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
